168ac7cf5d791441ac1d9aaf7b06108f988fa9ce
KatarinaH <katarina.hansson11@gmail.com>
2015-08-18 09:47:45 +0200

    index and menu

--- index.php
+++ index.php
@@ -3,16 +3,14 @@
 	include 'menu.php';  	
 ?>
 <div id="slider">
-	<div class="box">
-		<div id="left">
-			<i class="fa fa-caret-left"></i>
-		</div>
-		<div id="middle">
-			<img src="clothes/hip-man1.jpg" id="viewer">
-		</div>
-		<div id="right">
-			<i class="fa fa-caret-right"></i>
-		</div>
+	<div id="left">
+		<i class="fa fa-caret-left"></i>
+	</div>
+	<div id="middle">
+		<img src="clothes/hip-man1.jpg" id="viewer">
+	</div>
+	<div id="right">
+		<i class="fa fa-caret-right"></i>
 	</div>
 </div>
 
--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
 <div id="menu">
-	<img class="logo" src="img/logo5.png" alt="logo">
+	<a href="index.php"><img class="logo" src="img/logo5.png" alt="logo"></a>
 	<ul>
-		<li><a href="woman.php"> Kvinna </a></li>
-		<li><a href="man.php"> Man </a></li>
-		<li><a href="unisex.php"> Unisex </a></li>
-		<li><a href="info.php"> Info </a></li>
-		<li><a href="shoppingbag.php"> Kassa </a></li>
+		<li id="menu-info"><a href="info.php"> Info </a></li>
+		<li id="menu-woman"><a href="woman.php"> Kvinna </a></li>
+		<li id="menu-man"><a href="man.php"> Man </a></li>
+		<li id="menu-unisex"><a href="unisex.php"> Unisex </a></li>
+		<li id="menu-shop"><a href="shoppingbag.php"> Kassa </a></li>
 	</ul>
 </div>
