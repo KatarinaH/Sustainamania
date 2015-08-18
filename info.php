d00c66527cc5a013df70edf38f6f07a16fc21e3b
KatarinaH <katarina.hansson11@gmail.com>
2015-08-18 09:58:50 +0200

    info + style

--- info.php
+++ info.php
@@ -1,8 +1,85 @@
 <?php include 'header.php'; ?>
 <?php include 'menu.php';?>
-<p>I motsättning till vad många tror, är inte Lorem Ipsum slumvisa ord. Det har sina rötter i ett stycke klassiskt litteratur på latin från 45 år före år 0, och är alltså över 2000 år gammalt. Richard McClintock, en professor i latin på Hampden-Sydney College i Virginia, översatte ett av de mer ovanliga orden, consectetur, från ett stycke Lorem Ipsum och fann dess ursprung genom att studera användningen av dessa ord i klassisk litteratur. Lorem Ipsum kommer från styckena 1.10.32 och 1.10.33 av "de Finibus Bonorum et Malorum" (Ytterligheterna av ont och gott) av Cicero, skriven 45 före år 0. Boken är en avhandling i teorier om etik, och var väldigt populär under renäsanssen. Den inledande meningen i Lorem Ipsum, "Lorem Ipsum dolor sit amet...", kommer från stycke 1.10.32.
 
-Den ursprungliga Lorem Ipsum-texten från 1500-talet är återgiven nedan för de intresserade. Styckena 1.10.32 och 1.10.33 från "de Finibus Bonorum et Malorum" av Cicero hittar du också i deras originala form, åtföljda av de engelska översättningarna av H. Rackham från 1914.</p>
-		
+<div id="info-wrapper">
+		<h1>VÅRT NAMN</h1>
+
+		<!-- TOP ROW-->
+		<!--video-->
+		<div class="block3 grid-item">
+			<div class="video-container">
+				<iframe width="625px" height="360px" poster="../img/conscious.jpg" src="https://www.youtube.com/embed/cfcJVC_pnOk?autoplay=0&controls=0&showinfo=0&rel=0&#t=2s" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>
+		<!--#video--> 
+
+		<!--div top right-->
+		<div class="block1 grid-item box8">
+			<div class="cell-text">
+				<span>kampanjnamn</span>
+			</div>
+		</div>
+		<!--#div top right-->
+		<!--pop up box-->
+		<div class="overlay" id="overlay" style="display:none;"></div>
+
+		<div class="box" id="box">
+			<a class="boxclose" id="boxclose"><i class="fa fa-times"></i></a>
+			<h1>Kampanjnamn</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in sollicitudin leo. Curabitur neque erat, posuere ac ultricies eu, aliquam a lectus. Quisque commodo tristique risus sit amet interdum. In vitae lacus eu diam dignissim gravida. Quisque blandit nunc tempus nisl semper, non iaculis eros congue. Donec posuere dignissim orci, vitae consequat ante tincidunt ut. Vestibulum sapien massa, sodales scelerisque hendrerit sit amet, finibus nec est. Integer id diam vestibulum, lobortis tortor nec, molestie sapien. Etiam in bibendum arcu, ultricies ultrices libero. Duis sagittis pretium nibh, ac maximus libero maximus ut. Aenean at dapibus ligula, a sollicitudin ante. Vivamus dictum ultricies enim, sed vehicula magna. Phasellus eu placerat lacus, id fringilla dolor. Nam sem augue, pretium ac auctor ut, pellentesque elementum enim.
+			</p>
+			<p>
+				Fusce ac nunc vitae odio auctor lobortis. Cras et convallis tellus. Nulla vitae finibus augue, non tincidunt magna. Suspendisse quis ipsum sed leo volutpat ultrices. Mauris posuere, libero id dapibus dictum, risus lorem ullamcorper velit, eget tristique tortor odio et odio. Proin malesuada iaculis nibh in pharetra. Suspendisse nec ultricies orci. Mauris rhoncus iaculis tempor.
+			</p>
+			<div class="info-img">
+				<img class="popup" src="img/cotton.jpg">
+				<img class="popup" src="img/woman.jpg">
+				<img class="popup" src="img/clothes.jpg">
+			</div>
+		</div>
+		<!--#pop up box-->
+		<!-- #TOP ROW-->
+
+		<!-- BOTTOM ROW-->
+		<!--div bottom left-->
+		<div class="block1 grid-item">
+			<div class="block2 box5">
+				<div class="cell-text">
+					<span>arbetsförhållanden</span>
+				</div>
+			</div>
+			<div class="block2 box6">
+				<div class="cell-text">
+					<span>löner</span>
+				</div>
+			</div>
+		</div>
+		<!--#div bottom left-->
+
+		<!--div bottom middle-->
+		<div class="block1 grid-item box7">
+			<div class="cell-text">
+				<span>conscious</span>
+			</div>
+		</div>
+		<!--#div bottom middle-->
+
+		<!--div bottom right-->
+		<div class="block1 grid-item">
+			<div class="block2 box3">
+				<div class="cell-text">
+					<span>vatten</span>
+				</div>
+			</div>
+			<div class="block2 box4">
+				<div class="cell-text">
+					<span>bomull</span>
+				</div>
+			</div>
+		</div>
+		<!--#div bottom right-->
+		<!-- #BOTTOM ROW-->
+	</div>
 
 <?php include 'footer.php'; ?>
